56841efa076c10437c481fdf1e1067d229192f8e
Thomas Whitford <twhitford2224@gmail.com>
2024-06-26 00:50:12 +0100

    Start Of project

---
+++ UART_Reciever.srcs/sources_1/new/TOP.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: MinervaElectronics
+// Engineer: Thomas Whitford
+// 
+// Create Date: 26.06.2024 00:43:24
+// Design Name: 
+// Module Name: TOP
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TOP(
+
+    );
+endmodule
